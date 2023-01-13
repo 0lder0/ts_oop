85e0aab04491a53dc38285b9591ed5c5e10a93a4
Lilii <lilii-ann.older@voco.ee>
2023-01-13 09:29:01 +0200

    Harjutus 3.1 ja 3.2

--- resistorrun.ts
+++ resistorrun.ts
@@ -1,15 +1,24 @@
 class Resistor {
     r: number = 0;
-    constructor(r: number) {
+    m: number = 0
+    constructor(r: number, m: number) {
         this.r = r;
+        this.m = m;
+
     }
     getCurrent(u: number): number {
         return u / this.r;
     }
-    getPower(u: number): number {
-        return u * this.getCurrent(u);
+    getPower(u: number) {
+        const p =  u * this.getCurrent(u);
+        if (p <= this.m) {
+            return p
+        }
+        else {
+            console.log("-----------------")
+        }
     }
 }
 
-let r1: Resistor = new Resistor(1000);
-console.log(r1.getCurrent(220));
+let r1 = new Resistor(220, 100);
+console.log(r1.getPower(5));
